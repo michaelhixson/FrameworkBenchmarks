554543541e9c901fb0f1edadfe70e9d204b1dda5
Michael Hixson <mhixson@techempower.com>
2017-04-04 19:03:49 -0700

    Fix timeout in jawn fortunes endpoint

--- frameworks/Java/jawn/src/main/java/app/controllers/FortunesController.java
+++ frameworks/Java/jawn/src/main/java/app/controllers/FortunesController.java
@@ -13,11 +13,12 @@ public class FortunesController extends Controller {
 
     @Inject
     private DbManager db;
-    
+
     public void index() {
         List<Fortune> fortunes = db.fetchAllFortunes();
         fortunes.add(new Fortune(0, "Additional fortune added at request time."));
         Collections.sort(fortunes, (f1, f2) -> f1.message.compareTo(f2.message));
         view("fortunes", fortunes);
+        render().layout("../index");
     }
 }
