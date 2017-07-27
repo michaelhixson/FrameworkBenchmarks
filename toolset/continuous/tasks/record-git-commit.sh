0410a6ea82cdd02114bc30bdb97a0dceeb1dfa1d
Michael Hixson <mhixson@techempower.com>
2017-07-27 09:18:24 -0700

    Fix git commit id recording, part 2

--- toolset/continuous/tasks/record-git-commit.sh
+++ toolset/continuous/tasks/record-git-commit.sh
@@ -5,6 +5,6 @@
 echo "Saving current git commit to results directory..."
 GIT_COMMIT=$(git -C $TFB_REPOPARENT/$TFB_REPONAME rev-parse HEAD)
 mkdir -p $TFB_REPOPARENT/$TFB_REPONAME/results
-echo $GIT_COMMIT > results/.commit
+echo $GIT_COMMIT > $TFB_REPOPARENT/$TFB_REPONAME/results/.commit
 echo "Using commit: " $GIT_COMMIT
 
